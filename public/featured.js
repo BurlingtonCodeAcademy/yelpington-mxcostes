7b282e888d8753b661747158f65e627b6fa68750
Max-Costes <mxcostes@gmail.com>
2020-03-06 14:24:18 -0500

    featured page sidebar fills and map focuses on location pin

--- public/featured.js
+++ public/featured.js
@@ -1,16 +1,56 @@
+// path variables
 let path = window.location.pathname
 let pathArray = path.split('/')
 let id = pathArray.pop()
 console.log(id)
+//  funcitoning variables
+let title = document.getElementById('featured')
+title.textContent = id
 
 
-alert('js connected')
+// -----------enter name notes number address hours into sidebar-----
+window.fetch('https://json-server.burlingtoncodeacademy.now.sh/restaurants/' + id)
+    .then(response => response.json()) /* get date */
+    .then(json => {
+        let name = document.getElementById('name')
+        let notes = document.getElementById('notes')
+        let number = document.getElementById('number')
+        let address = document.getElementById('address')
+        let hours = document.getElementById('hours')
+        name.textContent = json.name
+        notes.textContent = json.notes.pop(0)
+        number.textContent = json.phone
+        address.textContent = json.address
+        hours.textContent = json.hours
+        dropPin(address.textContent, name.textContent)
 
-// map import 
-//gives map and starts at set coordinates and zoom
-let mymap = L.map('myMap').setView([44.47655925, -73.21435285244789], 16)
-//specifies the tiles you want to be shown. Theme.
-var OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    maxZoom: 50,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-}).addTo(mymap)
+    }
+    
+    )
+   
+    // -------------------drop pin and focus on location -----------------------------
+  function dropPin(address, name){  
+let latLon = {}
+    fetch(`https://nominatim.openstreetmap.org/search/?q=${address}&format=json`)
+.then((data) => {
+    return data.json()
+})
+.then((locInfo) => {
+    let info = locInfo[0]
+    console.log(locInfo[0])
+    let lat = info.lat
+    let lon = info.lon
+    latLon.lat = lat
+    latLon.lon = lon
+    let mymap = L.map('myMap').setView([lat, lon], 18)
+    var OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 50,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+    }).addTo(mymap)
+    L.marker([lat, lon]).addTo(mymap).bindPopup(name)
+    
+})
+
+  }
+  
+//   ----------------------commenting ------------------------------
